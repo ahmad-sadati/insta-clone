ad050549cb09c0a342e6ce0206d7382197eadbed
Ahmad Sadati <a.sadati336@gmail.com>
2023-11-26 12:54:16 +0330

    test1

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <?php echo "hello world";
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
 </body>
