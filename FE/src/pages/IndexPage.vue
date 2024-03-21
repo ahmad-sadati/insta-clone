73bb2e434dd23d4dfced7fb0555950196dca060c
Ahmad Sadati <a.sadati336@gmail.com>
2024-03-21 15:16:01 +0330

    added footer and few fixes on layout

--- FE/src/pages/IndexPage.vue
+++ FE/src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <div v-if="userData">
-      {{ appData.phone.number }}
+      {{ appData.userDetail.phone.number }}
     </div>
     <q-btn
       class="full-width"
